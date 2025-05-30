e0b92868a7dddfd639e45b3d79499937389ab496
0zuth <breadjoo1014@naver.com>
2025-05-30 17:58:54 +0900

    SCRUM-41 feat: 스크랩 버튼 추가

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -14,8 +14,7 @@ const buttonVariants = cva(
         secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
-        outlinePrimary:
-          'border-primary border bg-primary-foreground text-primary hover:bg-primary-foreground/90',
+        outlinePrimary: 'border-primary border bg-white text-primary hover:opacity-80',
       },
       size: {
         default: 'h-10 px-4 py-2',
